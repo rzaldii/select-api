0559e2ea22719283de463ea070703ae5c15640f2
Aden Alexandria <adenperdana1477@gmail.com>
2026-05-22 00:14:50 +0700

    mencoba deploy 2

--- api/index.ts
+++ api/index.ts
@@ -40,7 +40,56 @@ async function bootstrap() {
       .build();
 
     const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup('api/docs', app, document);
+
+    server.get('/api/docs-json', (_req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(document);
+    });
+
+    server.get(['/api/docs', '/api/docs/'], (_req, res) => {
+      res.setHeader('Content-Type', 'text/html');
+      res.send(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>SELECT API Docs</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+    <style>
+      html, body {
+        margin: 0;
+        padding: 0;
+        background: #ffffff;
+      }
+      #swagger-ui {
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
+    <script>
+      window.onload = function () {
+        window.ui = SwaggerUIBundle({
+          url: '/api/docs-json',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          persistAuthorization: true,
+          presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+          ],
+          layout: 'StandaloneLayout'
+        });
+      };
+    </script>
+  </body>
+</html>
+      `);
+    });
 
     await app.init();
     isInitialized = true;
